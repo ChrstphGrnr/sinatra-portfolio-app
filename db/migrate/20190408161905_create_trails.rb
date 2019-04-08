d73c95c80f9bd4df0d3c7f3b92d7eddea757d42b
ChrstphGrnr <ChristophGruener@me.com>
2019-04-08 10:34:07 -0600

    created trails table with user association

---
+++ db/migrate/20190408161905_create_trails.rb
@@ -0,0 +1,11 @@
+class CreateTrails < ActiveRecord::Migration
+  def change
+    create_table :trails do |t|
+      t.string :name 
+      t.string :description
+      t.integer :user_id
+
+      t.timestamps null: false
+    end
+  end
+end
