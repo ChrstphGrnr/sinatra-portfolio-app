da25c51816fa283742e044c1b0540873aea87298
ChrstphGrnr <ChristophGruener@me.com>
2019-04-10 13:23:10 -0600

    add trails routes

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -8,9 +8,20 @@ class TrailsController < ApplicationController
         erb :'trails/index.html'
     end
 
+    post '/trails' do 
+        binding.pry
+        trail = Trail.create(params)
+        redirect "/trails/#{trail.id}"
+    end
+    
+    get '/trails/:id' do 
+        @trail = Trail.find(params[:id])
+        erb :'/trails/show.html'      
+    end
+
+
     get "/trails/:id/edit" do 
         erb :'trails/edit.html'
-    
     end
 
     patch "/trails/:id" do
