1df13bb794293469beaec9e04d69e656250644f1
ChrstphGrnr <ChristophGruener@me.com>
2019-04-12 10:25:38 -0600

    change html layout of several views

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20190408161905) do
+
+  create_table "trails", force: :cascade do |t|
+    t.string  "name"
+    t.string  "description"
+    t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,6 @@
 
 User.destroy_all
 Trail.destroy_all
-Collection.destroy_all
 
 # Users
 
@@ -19,19 +18,9 @@ jack = User.create(name: "Jack", email: "jack@jack.com", password: "password")
 
 #Trails 
 
-Trail.create(name: "Blodget Peak", description: "This is the description of the trail", created_by_id: christoph.id)
-Trail.create(name: "Mount Herman", description: "This is another description of another trail", created_by_id: hillary.id)
-Trail.create(name: "Cannonball", description: "This is a crazy trail in the woods.", created_by_id: christoph.id)
-Trail.create(name: "Barr Trail", description: "There is no description yet.", created_by_id: jack.id)
-Trail.create(name: "Cheyenne Mountain Canyon", description: "This needs to be filled", created_by_id: elliott.id)
-
-# Collections
-
-collection_1 = Collection.create(name: "Summer2019", user_id: hillary.id)
-collection_2 = Collection.create(name: "Into the woods", user_id: christoph.id)
-
-user_trail_1 = UserTrail.create(user_id: 1, trail_id: 1)
-
-user_trail_1.collection_id = collection_1.id
-
+Trail.create(name: "Blodget Peak", description: "This is the description of the trail", user_id: christoph.id)
+Trail.create(name: "Mount Herman", description: "This is another description of another trail", user_id: hillary.id)
+Trail.create(name: "Cannonball", description: "This is a crazy trail in the woods.", user_id: christoph.id)
+Trail.create(name: "Barr Trail", description: "There is no description yet.", user_id: jack.id)
+Trail.create(name: "Cheyenne Mountain Canyon", description: "This needs to be filled", user_id: elliott.id)
 
