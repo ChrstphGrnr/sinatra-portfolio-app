e4fb65cbac390e6ff49cbe24f758c56d59504975
ChrstphGrnr <ChristophGruener@me.com>
2019-04-12 09:38:58 -0600

    make sure users can only edit/delete their own profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
 
  
-
-  # GET /login
   get '/login' do 
     erb :'users/login.html' 
   end
 
-  # POST /login
+
   post '/login' do 
-    # binding.pry
-    
     @user = User.find_by(email: params[:email])
     if !!@user && @user.authenticate(params[:password])
       # binding.pry
@@ -21,23 +17,22 @@ class UsersController < ApplicationController
     end
   end
 
-  # GET /signup
+
   get '/signup' do 
     erb :'users/new.html'
   end
 
   
-  # GET: /users
   get "/users" do
     erb :"/users/index.html"
   end
 
-  # GET: /users/new
+  
   get "/users/new" do
     erb :"/users/signup.html"
   end
 
-  # POST: /users
+ 
   post "/users" do
     # binding.pry
     if params[:name] != "" && params[:password] != "" && params[:email] != "" && !User.find_by(email: params[:email])
@@ -49,39 +44,40 @@ class UsersController < ApplicationController
     end    
   end
 
-  # GET: /users/5
-  # make sure only your showpage is shown and not others!
+ 
   get "/users/:id" do
-    if logged_in? 
-      erb :"/users/show.html"
-    else 
-      erb :'/users/login.html'
-    end
+      if logged_in? && current_user.id == params[:id]
+        erb :"/users/show.html"
+      else
+        redirect '/login'
+      end
   end
 
-  # GET: /users/5/edit
-  # make sure you can only edit your own page and not others!
+ 
   get "/users/:id/edit" do
-    erb :"/users/edit.html"
+      if logged_in? && current_user.id == params[:id]
+        erb :"/users/edit.html"
+      else 
+        redirect '/login' 
+      end  
   end
 
-  # PATCH: /users/5
+ 
   patch "/users/:id" do
     redirect "/users/:id"
   end
 
-  # DELETE: /users/5/delete
-  # make sure you can only delete your own page and not others! HELPER METHOD !
+  
   delete "/users/:id/delete" do
     redirect "/users"
   end
 
-  
 
   get '/signout' do 
     session.clear
     erb :'/users/signout.html'
   end
+  
   
   post '/signout' do 
     session.clear
