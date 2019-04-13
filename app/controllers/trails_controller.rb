4ab5c012636b7b3037fcdb4de7d31b267fa603d9
ChrstphGrnr <ChristophGruener@me.com>
2019-04-12 21:13:56 -0600

    fixed a wrong redirect route for patch /trail/:id

--- app/controllers/trails_controller.rb
+++ app/controllers/trails_controller.rb
@@ -14,8 +14,15 @@ class TrailsController < ApplicationController
 
     post '/trails' do 
         # binding.pry
-        trail = Trail.create(params)
-        trail.user_id = current_user.id 
+        filename = params[:file][:filename]
+        file = params[:file][:tempfile]
+
+        trail = Trail.create(name: params[:name], description: params[:description], user_id: current_user.id, image: params[:file][:filename])
+        
+        File.open("public/images/#{filename}", 'wb') do |f|
+            f.write(file.read)
+        end
+        
         redirect "/trails/#{trail.id}"
     end
     
@@ -35,9 +42,9 @@ class TrailsController < ApplicationController
     end
 
     patch "/trails/:id" do
-        trail = Trail.find(params[:id])
-        trail.update(params)
-        redirect '/trails/:id'    
+        trail = Trail.find_by(id: params[:id])
+        trail.update(name: params[:name], description: params[:description])
+        redirect "/trails/#{trail.id}"    
     end
 
     delete '/trails/:id' do 
